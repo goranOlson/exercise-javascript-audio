2a9962810d5b3914fcb4045fdb889d508fbaec9f
Göran Olson <goran@famolson.se>
2024-02-02 11:29:24 +0100

    added functionality for next, repeat and shuffle

--- script.js
+++ script.js
@@ -10,24 +10,31 @@ const paus = document.querySelector('.controller .paus');
 paus.addEventListener('click', pausClicked);
 
 const previous = document.querySelector('.controller .previous');
-previous.addEventListener('click', previousClicked);
+previous.addEventListener('click', playPreviousMelody);
+
+const nextSong = document.querySelector('.controller .next');
+nextSong.addEventListener('click', playNextMelody);
 
 const btnRepeat = document.querySelector('.controller .repeat');
 btnRepeat.addEventListener('click', repeatClicked);
 
+const btnShuffle = document.querySelector('.controller .shuffle');
+btnShuffle.addEventListener('click', shuffleClicked);
+
 const progressor = document.querySelector('.box-controller .progressor');
 const progressBar = document.querySelector('.progress-bar');
 const timeDuration = document.querySelector('.progress-time');
-
 const audio = document.querySelector('audio');
 
 
+let doRepeat = false;
+let doShuffle = false;
 
 
 audio.onloadedmetadata = function() {
     // console.log('Time: ' + audio.duration);
 
-    document.querySelector('.melody-lenght').innerText = getTimes(audio.duration);
+    document.querySelector('.melody-lenght').innerText = getTimeString(audio.duration);
 };
 
 audio.addEventListener("timeupdate", function() {
@@ -40,44 +47,64 @@ audio.addEventListener("timeupdate", function() {
     
     progressBar.style.width = progressor.offsetWidth * (currentTime / duration) + "px";
 
-    const timePlayed = getTimes(currentTime);
+    const timePlayed = getTimeString(currentTime);
     timeDuration.innerText = (currentTime != NaN) ? timePlayed : 0;
 });
 
 audio.addEventListener("ended", function() {
     console.log('Song ended');
-    // if (loop)
-    // if (random)
-    //
+
     const actItem = document.querySelector('.song-list .item.active');
 
-    if (actItem.nextElementSibling) {
-        console.log('Next song...' + actItem.nextElementSibling.children[1].innerText);
+    if (doRepeat) {
+        actItem.click();
+    }
+    else if (doShuffle) {
+        // >= 2 melodies
+        //  console.log(createRandom(1, 8, 6));
+        // const nextNbr = createRandom(1, playList.children.length, NOTid);
+        // 
+        // actItem.dataset['melody']
 
-        actItem.classList.remove('active');
-        actItem.nextElementSibling.click();
+
+        console.log(actItem.dataset['melody']);
+        
+        // actItem index in playList
+        const idx = findIndex(playList.children, actItem.dataset['melody']);
+
+        // Warning: starts with 1...
+        const nextId = createRandom(1, playList.children.length, idx + 1);
+        // console.log('nextId: ' + nextId + ' =>' + (nextId - 1));  // 0-start...
+
+        if (nextId >= 0) {
+            // console.log('nextId: ' + nextId + ' => ' + (nextId - 1) );
+            // Correct index from createRandom
+            playList.children.item(nextId - 1).click();
+        }
+    }
+    else {
+        if (actItem.nextElementSibling) {
+            console.log('Next song...' + actItem.nextElementSibling.children[1].innerText);
+
+            actItem.classList.remove('active');
+            actItem.nextElementSibling.click();
+        }
     }
 });
-
-// audio.onplay = function() {
-//     console.log('onplay: ' + audio.duration);
-// }
-
-
-
 
 function songClicked(event) {
      console.log('--> songClicked()');
     // console.log(event.target);
 
     // Unactivate previous melody
-    const itemBefore = document.querySelector('.item.active');
-    if (itemBefore) {
-        itemBefore.classList.remove('active');
+    const melodyBefore = document.querySelector('.item.active');
+    if (melodyBefore) {
+        melodyBefore.classList.remove('active');
     } 
     
-    // Lighten up player
-    if ( !itemBefore ) {
+    // Lighten up player if no previous melody
+    if ( !melodyBefore ) {
+        document.querySelector('.player .box-image img').classList.add('active');
         document.querySelector('.player .song').classList.add('active');
         document.querySelector('.player .progress').classList.add('active');
         document.querySelector('.player .controller').classList.add('active');
@@ -85,10 +112,9 @@ function songClicked(event) {
     
     // Show info about new melody
     const item = event.target.closest('.item');
-
     item.classList.add('active');
-    // Set audio source
-    audio.src = 'assets/' + item.getAttribute("data-melody");  
+    audio.src = 'assets/' + item.getAttribute("data-melody");
+
     // Update player
     document.querySelector('.player img').src = item.children[0].src;
     document.querySelector('.player .artist').innerText = item.querySelector('.artist').innerText;
@@ -119,7 +145,7 @@ function pausClicked(event) {
     audio.pause();
 }
 
-function previousClicked(event) {
+function playPreviousMelody(event) {
      console.log('clicked previous');
     const actItem = document.querySelector('.item.active');
 
@@ -128,27 +154,97 @@ function previousClicked(event) {
             actItem.previousElementSibling.click();
         }
         else {
-            const allItems = document.querySelector('.song-list');
-
-            // Most be anoter melody then the playing to switch to...
-            if (allItems.children.length >= 2) {
-                allItems.children.item( allItems.children.length - 1 ).click();
+            if (playList.children.length >= 2) {
+                playList.children.item( playList.children.length - 1 ).click();
             }
         }
     }
-     else {
-        console.log('No actItem');
-    }
+}
+
+function playNextMelody(event) {
+    console.log('clicked next');
+   const actItem = document.querySelector('.item.active');
+
+   if (actItem) {
+       if (actItem.nextElementSibling) {
+           actItem.nextElementSibling.click();
+       }
+       else {
+           // Most be anoter melody then the playing to switch to...
+           if (playList.children.length >= 2) {
+            playList.children.item(0).click();
+           }
+       }
+   }
 }
 
 function repeatClicked(event) {
+    // console.log('--> repeatClicked() doRepeat: ' + doRepeat);
     
+    // Must have active melody
+    if (document.querySelector('.item.active')) {
+        if (doRepeat) {
+            doRepeat = false;
+            btnRepeat.classList.remove('active');
+            // console.log('doRepeat: ' + doRepeat);
+        }
+        else {
+            doRepeat = true;
+            btnRepeat.classList.add('active');
+            btnShuffle.classList.remove('active');
+            // console.log('doRepeat: ' + doRepeat);
+        }
+    }
+}
+
+function shuffleClicked(event) {
+    // console.log('--> shuffleClicked() doShuffle: ' + doShuffle);
+
+    // Must have active melody
+    if (document.querySelector('.item.active')) {
+        if (doShuffle) {
+            doShuffle = false;
+            btnShuffle.classList.remove('active');
+            // console.log('doShuffle: ' + doShuffle);
+        }
+        else {
+            doShuffle = true;
+            btnShuffle.classList.add('active');
+            btnRepeat.classList.remove('active');
+            // console.log('doShuffle: ' + doShuffle);
+        }
+    }
 }
 
 
-function getTimes(seconds) {
+function getTimeString(seconds) {
     return new Date(seconds * 1000).toISOString().slice(14, 19);
 }
+
+function createRandom(min, max, avoid) {
+    let nbr;
+    //  console.log('--> createRandom('+min+', '+max+' not '+avoid+')');
+    do {
+        nbr = Math.floor(Math.random() * (max - min + 1) + min);    
+        // console.log(`Test: ${nbr} vs ${avoid}`);
+    } while (nbr === avoid);
+
+    return nbr;
+}
+
+function findIndex(htmlCollection, string) {
+    let index = -1;
+
+    for (let i = 0; i < htmlCollection.length; i++) {
+        if (htmlCollection[i].dataset['melody'] === string) {
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+}
+
 
 
 //=== TEST ===
